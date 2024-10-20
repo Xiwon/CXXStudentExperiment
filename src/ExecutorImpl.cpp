ee02b9bd4ebe6d352bd9e4642c07a4d0422bfcc9
Xiwon <3550216663@qq.com>
2024-10-20 12:49:47 +0800

    test: Pass turn right command test cases

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -58,6 +58,25 @@ namespace adas
                     pose.heading = 'E';
                 }
             }
+            else if (cmd == 'R')
+            {
+                if (pose.heading == 'E')
+                {
+                    pose.heading = 'S';
+                }
+                else if (pose.heading == 'S')
+                {
+                    pose.heading = 'W';
+                }
+                else if (pose.heading == 'W')
+                {
+                    pose.heading = 'N';
+                }
+                else if (pose.heading == 'N')
+                {
+                    pose.heading = 'E';
+                }
+            }
         }
     }
 }
